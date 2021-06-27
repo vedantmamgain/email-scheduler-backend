530fbe3216ce5f86aaf4bb99978f4cf71b7c6ea5
Vedant Mamgain <mamgainvedant@gmail.com>
2021-06-27 22:26:16 +0530

    asd

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const app = express();
 const morgan = require("morgan");
 const router = require("./routes/emailRoutes");
 const path = require("path");
-
+const cors = require("cors");
 require("dotenv").config();
-app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "/client/build")));
+
 // app.use(express.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+    res.render("index", { title: "Express" });
+});
+
 app.use("/api/", router);
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 module.exports = app;
