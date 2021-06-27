a16becfd74725b260170a64660d83628761a8117
Vedant Mamgain <mamgainvedant@gmail.com>
2021-06-27 20:24:07 +0530

    Chnaged scheduler format

--- util/scheduler.js
+++ util/scheduler.js
@@ -2,30 +2,36 @@ const schedule = require("node-schedule");
 const postHistory = require("../controller/emailController");
 //Every 20 or 30 seconds
 module.exports = async function (data) {
-    let seconds = data.body.seconds;
-    let weekly = data.body.weekly;
-    let monthly = data.body.monthly;
+    let seconds = data.body.recurring;
+    let weekly = data.body.week;
+    let monthly = data.body.month;
+    let yearly = data.body.year;
 
-    if (seconds) {
-        if (data.body.time.seconds == 20) {
-            schedule.scheduleJob("*/2 * * * * *", async function () {
+    if (seconds.schedule) {
+        if (seconds.dateAndTime == 20) {
+            schedule.scheduleJob("*/20 * * * * *", async function () {
                 await postHistory.postHistory(data);
-                console.log("sending a mail every 2 seconds");
+                console.log("sending a mail every 20 seconds");
             });
         } else {
-            schedule.scheduleJob("*/3 * * * * *", async function () {
+            schedule.scheduleJob("*/30 * * * * *", async function () {
                 await postHistory.postHistory(data);
-                console.log("sending a mail every 3 seconds");
+                console.log("sending a mail every 30 seconds");
             });
         }
     }
-    if (weekly) {
+    if (weekly.schedule) {
         //Weekly
+        let data = new Date(weekly.dateAndTime);
+        let date = data.getDate();
+        let minutes = data.getMinutes();
+        let hour = data.getHours();
+
         schedule.scheduleJob(
             {
-                hour: data.body.time.hours,
-                minute: data.body.time.minute,
-                dayOfWeek: data.body.time.day,
+                hour: hour,
+                minute: minutes,
+                dayOfWeek: date,
             },
             async function () {
                 await postHistory.postHistory(data);
@@ -40,13 +46,18 @@ module.exports = async function (data) {
             }
         );
     }
-    if (monthly) {
+    if (monthly.schedule) {
+        let data = new Date(weekly.dateAndTime);
+        let date = data.getDate();
+        let minutes = data.getMinutes();
+        let hour = data.getHours();
+
         //monthly
         var rule = new schedule.RecurrenceRule();
         rule.month = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-        rule.date = data.body.date;
-        rule.hour = data.body.time.hour;
-        rule.minute = data.body.time.minute;
+        rule.date = date;
+        rule.hour = hour;
+        rule.minute = minutes;
 
         schedule.scheduleJob(rule, async function () {
             await postHistory.postHistory(data);
@@ -60,18 +71,15 @@ module.exports = async function (data) {
             );
         });
     }
-    if (yearly) {
+    if (yearly.schedule) {
+        let data = new Date(weekly.dateAndTime);
+        let date = data.getDate();
+        let minutes = data.getMinutes();
+        let hour = data.getHours();
+        let month = data.getMonth() + 1;
         //yearly
         let cronString =
-            data.body.time.minute +
-            " " +
-            data.body.time.hour +
-            " " +
-            data.body.time.date +
-            " " +
-            data.body.time.month +
-            " " +
-            "*";
+            minutes + " " + hour + " " + date + " " + month + " " + "*";
         schedule.scheduleJob(cronString, async function () {
             await postHistory.postHistory(data);
             console.log("sending a mail yearly at" + cronString);
