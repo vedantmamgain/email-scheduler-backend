86331cdeb3c1f87c70655e208714c53cf01be87f
Vedant Mamgain <mamgainvedant@gmail.com>
2021-06-27 21:45:37 +0530

    React front end Added

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const router = require("./routes/emailRoutes");
+const path = require("path");
 
 require("dotenv").config();
-
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(morgan("dev"));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 app.use("/api/", router);
 
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 module.exports = app;
