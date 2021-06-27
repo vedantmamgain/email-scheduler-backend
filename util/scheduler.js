4bad183e1b10d593400500d530c7ba6055e9b8e9
Vedant Mamgain <mamgainvedant@gmail.com>
2021-06-27 17:37:13 +0530

    Changes

--- util/scheduler.js
+++ util/scheduler.js
@@ -1,9 +1,1 @@
-//run postHistory
-const cron = require("node-cron");
-
-module.exports = async function (options) {
-    cron.schedule("* * * * *", async function () {
-        console.log("Running this even after API Request the cron job");
-        await main(arr);
-    });
-};
+const schedule = require("node-schedule");
